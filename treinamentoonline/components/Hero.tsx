9b1075fc287157dbedd856533c253394209325e9
wagner matheus fonteles alves <wagnermatheus1900188@gmail.com>
2025-06-17 11:08:07 -0300

    Add promotional text to Hero banner

--- treinamentoonline/components/Hero.tsx
+++ treinamentoonline/components/Hero.tsx
@@ -6,6 +6,12 @@ export function Hero() {
     <section className="hero text-center p-10">
       <Image src={dragonSrc} alt="Dragão oriental" className="dragon mx-auto" />
       <h2 className="text-2xl mt-4">Desenvolva suas habilidades e aumente seus conhecimentos</h2>
+      <p className="mt-4 max-w-2xl mx-auto">
+        Tenha acesso ao treinamento de artes marciais online mais abrangente, do
+        iniciante ao instrutor! Agora você tem a oportunidade de conhecer passo a
+        passo, programa por programa, sobre a arte de Bruce Lee! Conheça o poder
+        do Jeet Kune Do, considerado &quot;O original Mixed Martial Arts&quot;
+      </p>
     </section>
   );
 }
