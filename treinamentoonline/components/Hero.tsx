c49fa67dff0121be6a6f89a8a6b97402b80e19d4
wagner matheus fonteles alves <wagnermatheus1900188@gmail.com>
2025-06-16 21:43:37 -0300

    Update Hero.tsx

--- treinamentoonline/components/Hero.tsx
+++ treinamentoonline/components/Hero.tsx
@@ -1,7 +1,7 @@
 export function Hero() {
   return (
     <section className="hero text-center p-10">
-      <img src="/images/dragon.svg" alt="Dragão oriental" className="dragon mx-auto" />
+      <img src="/images/20250616_2132_Dragão Oriental Intimidador_simple_compose_01jxxmqkb1f39tvf3mzjkg1xb6.png" alt="Dragão oriental" className="dragon mx-auto" />
       <h2 className="text-2xl mt-4">Desenvolva suas habilidades e aumente seus conhecimentos</h2>
     </section>
   );
